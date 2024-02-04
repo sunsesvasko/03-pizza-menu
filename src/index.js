9699df20449b4f7539fb1b4d37912842b74d8a28
Vasko <vbblajev@gmail.com>
2024-02-04 16:59:32 +0200

    Putting a react fragment

--- src/index.js
+++ src/index.js
@@ -81,19 +81,25 @@ function Menu() {
       {/* Conditional Rendering with && */}
       {/* {anyPizzas && (
         <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
-        </ul>
-      )} */}
+          </ul>
+        )} */}
 
       {/* Conditional Rendering with Ternary ? : */}
       {anyPizzas ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later!</p>
       )}
